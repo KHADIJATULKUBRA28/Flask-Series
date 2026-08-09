13e11e5b718a6a52c3d772f7c97694fcad767a85
Khadijatul Kubra <khadijatul.kubraa28@gmail.com>
2026-08-09 21:43:48 +0530

    1 hr 21 min : template inheritance

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -10,7 +10,7 @@ def home_page():
 def market_page():
     items = [
         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '234567890123', 'price': 988},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '345678901234', 'price': 150}
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
     return render_template('market.html', items=items)
