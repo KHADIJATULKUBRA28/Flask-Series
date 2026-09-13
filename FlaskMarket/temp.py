9960619deee1ad50a679b3899cb3f5d8f949dccd
Khadijatul Kubra <khadijatul.kubraa28@gmail.com>
2026-09-13 22:11:02 +0530

    database

--- FlaskMarket/temp.py
+++ FlaskMarket/temp.py
@@ -1,3 +1,9 @@
-from market import db, app
+from market import db, app, Item
+
 with app.app_context():
     db.create_all()
+    item2 = Item(name="Samsung S30 Ultra", price=1500, barcode='797239742989', description='The best Samsung phone ever made')
+    item3 = Item(name="iPhone 20", price=2000, barcode='797239742999', description='The latest and greatest iPhone')
+    db.session.add(item2)
+    db.session.add(item3)
+    db.session.commit()
