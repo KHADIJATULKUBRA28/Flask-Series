cf4455497fbce9e3c0693cb651061e5b1f083e21
Khadijatul Kubra <khadijatul.kubraa28@gmail.com>
2026-09-14 20:40:47 +0530

    forms

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ab3e5f321874fa6cf9a39ede'
 db = SQLAlchemy(app)
-
 from market import routes
-from market import models
--- FlaskMarket/temp.py
+++ FlaskMarket/temp.py
@@ -3,9 +3,9 @@ from market.models import Item
 
 with app.app_context():
     db.create_all()
-    item1 = Item(name="iPhone 18", price=2000, barcode='797239742979', description='The best phone in the world')
+    item1 = Item(name="iPhone 17", price=2000, barcode='797239742979', description='The best phone in the world')
     item2 = Item(name="Samsung S30 Ultra", price=1500, barcode='797239742989', description='The best Samsung phone ever made')
-    item3 = Item(name="iPhone 20", price=2000, barcode='797239742999', description='The latest and greatest iPhone')
+    item3 = Item(name="iPhone 18", price=2000, barcode='797239742999', description='The latest and greatest iPhone')
     db.session.add(item1)
     db.session.add(item2)
     db.session.add(item3)
